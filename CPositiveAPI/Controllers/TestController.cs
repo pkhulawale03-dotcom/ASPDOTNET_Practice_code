8a7734d082c147e205072e8a21f48217a57a85a1
pkhulawale03-dotcom <pkhulawale03@gmail.com>
2025-09-15 13:54:29 +0530

    Initial commit from new account

---
+++ CPositiveAPI/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using CPositiveAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CPositiveAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public readonly ApplicationDbContext Context;
+        public TestController(ApplicationDbContext applicationDbContext)
+        {
+            applicationDbContext = Context;
+        }
+
+        [HttpGet]
+        public ActionResult get() 
+        {
+            try
+            {
+                var getdata= Context.PatientDetails.Include(p => p.PatientId).ToList();
+                if(getdata == null)
+                {
+                    return NotFound();
+                }
+                return Ok(getdata);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
